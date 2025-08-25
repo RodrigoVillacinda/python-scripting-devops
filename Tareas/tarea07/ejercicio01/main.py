d34e91d9979a65c8e2d9f3ed359e15bd9d3525e2
RodrigoVillacinda <ligoche13@gmail.com>
2025-08-25 17:06:03 -0600

    Agrega ejercicio01 de tarea07 con estructura base

---
+++ Tareas/tarea07/ejercicio01/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+def main():
+    myList = []
+    x = 0
+    print("------Input:------")
+    while x != -1:
+        x=int(input("Ingrese un numero: "))
+        if x == -1:
+            break
+        myList.append(int(x))
+
+    i = 0
+    sum = 0
+    for i in myList:
+        sum += i
+        print(i) 
+    
+    sum = sum/len(myList)
+    sum = int(sum)
+    myList.insert(0, sum)
+
+    j = 0
+    print("------Output:------")
+    for j in myList:
+        print(j)
+
+
+if __name__ == "__main__":
+    main()
